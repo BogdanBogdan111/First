7071443685c6253ae7807689b2acc061cd02394e
Bogdan <legor3455@gmail.com>
2022-10-01 22:34:12 +0300

    Задание готово

--- main.js
+++ main.js
@@ -1,9 +1,7 @@
 /* Шаг 1 */
 let numberOfFilms = prompt("Сколько фильмов вы уже посмотрели?", 0);
-console.log(numberOfFilms);
 
 /* Шаг 2 */
-
 const personalMovieDB = {
   count: numberOfFilms,
   movies: {},
@@ -11,5 +9,17 @@ const personalMovieDB = {
   genres: {},
   privat: false,
 };
+
+/* Шаг 3 */
+const lastViewedFilmName = prompt(
+    "Один из просмотренных фильмов",
+    "Неизвестно"
+  ),
+  lastViewedFilmRate = prompt("На сколько оцените его?", "Неизвестно"),
+  lastViewedFilmName2 = prompt("Один из просмотренных фильмов", "Неизвестно"),
+  lastViewedFilmRate2 = prompt("На сколько оцените его?", "Неизвестно");
+
+personalMovieDB.movies[lastViewedFilmName] = lastViewedFilmRate;
+personalMovieDB.movies[lastViewedFilmName2] = lastViewedFilmRate2;
 
 console.log(personalMovieDB);
