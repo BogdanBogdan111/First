09cbaa547c7313807ce4384c15ea491ae633fcb4
Bogdan <legor3455@gmail.com>
2022-10-01 22:22:39 +0300

    Шаг второй

--- main.js
+++ main.js
@@ -1,3 +1,15 @@
+/* Шаг 1 */
 let numberOfFilms = prompt("Сколько фильмов вы уже посмотрели?", 0);
-
 console.log(numberOfFilms);
+
+/* Шаг 2 */
+
+const personalMovieDB = {
+  count: numberOfFilms,
+  movies: {},
+  actors: {},
+  genres: {},
+  privat: false,
+};
+
+console.log(personalMovieDB);
